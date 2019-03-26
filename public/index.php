45b652781f904b516da9c9e3cf3418d643df1cbb
Anteunis Charles <charles.anteunis@gmail.com>
2019-03-26 09:37:47 +0100

    le titre!

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ $users = $userRepository->fetchAll();
 <head>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<title>Les spots Parkour !</title>
+<title>Move it !</title>
 </head>
 <body>
 
