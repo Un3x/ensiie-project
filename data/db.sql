cd16fdcebea8cb840dc9dd907939d080868ca071
Matthieu <matthieu@MacBook-Pro-de-Matthieu.local>
2019-03-26 12:42:52 +0100

    ajout table photo_profil

--- data/db.sql
+++ data/db.sql
@@ -3,9 +3,19 @@ CREATE TABLE "user" (
     firstname VARCHAR NOT NULL ,
     lastname VARCHAR NOT NULL ,
     birthday date
+    id_photo REFERENCES photo_profil(id)
+    city VARCHAR NOT NULL
+    mdp VARCHAR
 );
 
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('John', 'Doe', '1967-11-22');
+CREATE TABLE "photo_profil"(
+    id SERIAL PRIMARY KEY,
+    adresse VARCHAR NOT NULL
+);
+
+
+
+/*INSERT INTO "user"(firstname, lastname, birthday) VALUES ('John', 'Doe', '1967-11-22');
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Yvette', 'Angel', '1932-01-24');
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Amelia', 'Waters', '1981-12-01');
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Manuel', 'Holloway', '1979-07-25');
@@ -16,4 +26,4 @@ INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Vicky', 'Pearson', '1
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Silvia', 'Mcguire', '1971-03-02');
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Brendan', 'Pena', '1950-02-17');
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jackie', 'Cohen', '1967-01-27');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Delores', 'Williamson', '1961-07-19');
+INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Delores', 'Williamson', '1961-07-19');*/
