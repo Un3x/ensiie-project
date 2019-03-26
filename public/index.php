f57ebc5cec9e9bcc992492074c4c42fbf757c336
Anteunis Charles <charles.anteunis@gmail.com>
2019-03-26 09:20:16 +0100

    add all files

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php
+require '../vendor/autoload.php';
+//require '../src/User/UserRepository.php';
+//require '../src/User/User.php';
+
+//postgres
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPassword = getenv('DB_PASSWORD');
+$connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+
+$userRepository = new \User\UserRepository($connection);
+$users = $userRepository->fetchAll();
+?>
+
+<html>
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<title>Les spots Parkour !</title>
+</head>
+<body>
+
+<div class="container">
+    <h3><?php echo 'Hello world from Docker! php' . PHP_VERSION; ?></h3>
+
+    <table class="table table-bordered table-hover table-striped">
+        <thead style="font-weight: bold">
+            <td>#</td>
+            <td>Firstname</td>
+            <td>Lastname</td>
+            <td>Age</td>
+        </thead>
+      <?php /** @var \User\User $user */ 
+        foreach ($users as $user) : ?>
+            <tr>
+                <td><?php echo $user->getId() ?></td>
+                <td><?php echo $user->getFirstname() ?></td>
+                <td><?php echo $user->getLastname() ?></td>
+                <td><?php echo $user->getAge() ?> years</td>
+            </tr>
+      <?php endforeach; ?>
+
+    </table>
+</div>
+</body>
+</html>
