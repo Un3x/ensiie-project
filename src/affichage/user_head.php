dc8b76da3218db96c0710e0e4f5cb51d94670c32
Nicolas_Makaroff <nicolas.makaroff@sfr.fr>
2019-03-31 17:13:09 +0200

    Commencement du design
    j'ai fait pas mal de test 
    feuille css non reconnue je sais pas pourquoi Pour le moment il faut les importer manuellement dans le nav web

---
+++ src/affichage/user_head.php
@@ -0,0 +1,46 @@
+<?php   
+
+function enTete($titre)
+{
+    print "<!DOCTYPE html>\n";
+    print "<html>\n";
+    print "  <head>\n";
+    print "    <meta charset=\"utf-8\" />\n";
+    print "    <title>$titre</title>\n";
+    print "    <link rel=\"stylesheet\" type=\"text/css\" href=\"../src/affichage/user_head.css\"/>\n";
+    print "      <a href=\"signin.php\"> Sign In </a>";
+    print "  </head>\n";
+  
+    print "  <body>\n";
+    print "    <header><h1> $titre </h1></header>\n";
+}
+
+function pied(){
+        print "</body>";
+        print "</html>";
+}
+
+
+
+function aside(){
+    
+}
+
+function navigation(){
+    echo '
+    <nav>
+            <a href="index.php"> Home </a>
+            <a href="cat1.php"> Cat_1 </a>
+            <a href="cat2.php"> Cat_2 </a>
+            <a href="cat3.php"> Cat_3 </a>
+            <a href="contact.php"> Contact </a>
+            <a href="aboutus.php"> About Us </a>
+            <a href="../src/User/signin.php"> Sign In </a>
+            <button onclick="document.getElementById(\'id01\').style.display=\'block\'" style="width:auto;">Login</button>
+    </nav>
+    ';
+}
+
+
+
+?>
