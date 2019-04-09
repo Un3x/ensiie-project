dd8cc5ad7499ad08c40ebc81e4c1bf4f35282d50
acherouf2018 <yacine.acheroufkebir@gmail.com>
2019-04-09 17:57:06 +0200

    page d'accueuil

--- public/index.php
+++ public/index.php
@@ -1,43 +1,65 @@
 <?php
-require '../vendor/autoload.php';
 
-//postgres
-$dbName = getenv('DB_NAME');
-$dbUser = getenv('DB_USER');
-$dbPassword = getenv('DB_PASSWORD');
-$connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
-
-$userRepository = new \User\UserRepository($connection);
-$users = $userRepository->fetchAll();
 ?>
 
 <html>
 <head>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../CSS/cadre.css">
+	<link rel="stylesheet" href="../CSS/style.css">
 </head>
 <body>
 
-<div class="container">
-    <h3><?php echo 'Hello world from Docker! php' . PHP_VERSION; ?></h3>
 
-    <table class="table table-bordered table-hover table-striped">
-        <thead style="font-weight: bold">
-            <td>#</td>
-            <td>Firstname</td>
-            <td>Lastname</td>
-            <td>Age</td>
-        </thead>
-        <?php /** @var \User\User $user */
-        foreach ($users as $user) : ?>
-            <tr>
-                <td><?php echo $user->getId() ?></td>
-                <td><?php echo $user->getFirstname() ?></td>
-                <td><?php echo $user->getLastname() ?></td>
-                <td><?php echo $user->getAge() ?> years</td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+<h2>NOS ACTIVITES</h2>
+
+
+<div id="conteneur">
+<div class="bloc-half">
+<div>
+<h3>DES TUTOS</h3>
+<p>Nous avons pour vocation de créer des jeux videos, en 2D ou en 3D,
+Si votre choix de carrière est déjà porté sur le développement 
+de jeux, nourangs des utilisateurs habiles de Blender et Unity, qui 
+ont à leur actif plusieurs projets. </p>
 </div>
+
+<div>
+<h3>DU MATOS</h3>
+<p>L'association REALITIIE a également accès à la Platech, et donc
+à tous le matériel qui s'y touve(ou presque). Pour ceux qui sont intéréssés
+par la VR, le VIVE est accés vous invitons à nous rejoindre. Dans l'association
+vous aurez l'occasion de vous familiariser avec l'environnement
+de travail des amateurs comme pour  les pros, nous avons notamment
+dans nos ssible aux étudiants, que se soit pour tester leurs
+propres projets, ou pour améliorer son propre score sur Beatsaber.</p>
+</div>
+</div>
+
+
+<div class="bloc-half">
+<div>
+<h3>LAVAL</h3>
+<p>Avoir du matériel, c'est bien, savoir s'en servir c'est mieux.
+C'est dans cette optique qu'on encourage nos membres à s'inscrire 
+pour la game jam organisée chaque année à Laval, ou encore à présenter 
+leurs propres jeux réalisés durant l'année. Les participants
+peuvent profiter de la plus grande exposition de réalité virtuelle
+en Europe.</p>
+</div>
+</br>
+<div>
+<h3>LES PROJETS</h3>
+<p>Vous trouverez sur cette page les différents projets réalisés par
+les memebres de Realitiie. Les projets étant en accès libre, vous pourrez 
+vous même les installer et y jouer. On vous invite donc à les tester et 
+à laisser des avis constructifs.</p>
+</div>
+</div>
+</div>
+
+
+
+
 </body>
 </html>
