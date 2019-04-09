7f6fbdde49b029ed9cd85bb9fa5742be968f31a9
Paul FLEURANCE <paul.fleurance@pc131-08.pedago.ensiie.fr>
2019-04-09 17:18:12 +0200

    ébauche de pages

---
+++ public/editer.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+     </head>
+     <body>
+     <!--ALED TODO mettre le logo de sciience comme dans toutes les autres pages ainsi qu\'une petite page de garde sympathique-->
+     <h1>Page d\'édition du profil</h1>
+     <form>
+     <input type="text" name="Nouveau prenom"/>
+     <input type="text" name="Nouveau nom"/>
+     <input type="text" name="Nouveau pseudo"/>
+     <input type="text" name="Nouveau mot de passe"/>
+     <input type="text" name="Confirmation nouveau mot de passe"/>
+     <input type="text" name="Nouvelle date de naissance"/>
+     <input type="text" name="Nouvel email"/>
+     <input type="submit" name="Envoyer" value="Rendu"/>
+     </form>
+     </body>
+     </html>
