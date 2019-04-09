ce9030b3fb937cfc09d2a46c3306f71066e4883c
Andrieu Carla <carlaandrieu8@gmail.com>
2019-04-09 10:35:04 +0000

    Update db.sql

--- ensiie-project-master/data/db.sql
+++ ensiie-project-master/data/db.sql
@@ -1,19 +1,33 @@
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY ,
-    firstname VARCHAR NOT NULL ,
-    lastname VARCHAR NOT NULL ,
-    birthday date
-);
+-- ***************************
+-- * Création tables
+-- ***************************
+CREATE TABLE Utilisateur (
+       id SERIAL PRIMARY KEY,
+       nom VARCHAR(50) NOT NULL ,
+       prenom VARCHAR(50) NOT NULL ,
+       email VARCHAR(50) NOT NULL ,
+       password VARCHAR(50) UNIQUE NOT NULL ,
+       tel INTEGER ,
+       genre CHAR(1),
+       sport VARCHAR(50) NOT NULL );
+       
+-- Héritage (Classe Mère)
 
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('John', 'Doe', '1967-11-22');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Yvette', 'Angel', '1932-01-24');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Amelia', 'Waters', '1981-12-01');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Manuel', 'Holloway', '1979-07-25');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Alonzo', 'Erickson', '1947-11-13');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Otis', 'Roberson', '1995-01-09');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jaime', 'King', '1924-05-30');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Vicky', 'Pearson', '1982-12-12)');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Silvia', 'Mcguire', '1971-03-02');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Brendan', 'Pena', '1950-02-17');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jackie', 'Cohen', '1967-01-27');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Delores', 'Williamson', '1961-07-19');
+CREATE TABLE Jury ( ) INHERITS (Utilisateur);
+CREATE TABLE Organisateur ( ) INHERITS (Utilisateur);
+CREATE TABLE Participant ( ) INHERITS (Utilisateur);
+
+CREATE TABLE Sport (
+       nom VARCHAR(50) PRIMARY KEY ,
+       lieu VARCHAR(50) NOT NULL ,
+       equipe VARCHAR(50) ,
+       genre CHAR(1) NOT NULL );
+
+CREATE TABLE Logement (
+       adresse VARCHAR(100) NOT NULL,
+       numero INTEGER,
+       num_lit INTEGER,
+       PRIMARY KEY ( adresse , numero, num_lit ),
+       locataire INTEGER  REFERENCES Utilisateur(id),
+       type_log VARCHAR(50) ); 
+
