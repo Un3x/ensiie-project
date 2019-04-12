eef7441471d54f2349cf5c1bfe8abd5249362d3c
Matteo BRANDI <matteo.brandi@ensiie.fr>
2019-04-12 19:53:00 +0200

    page d'administration

--- public/admin.php
+++ public/admin.php
@@ -14,11 +14,15 @@ if(isset($_SESSION['pseudo'])){ //Si pas connecté, renvoie vers la page de conn
 
 <h1>Page d'administration</h1>
 
-<h2>Bienvenue sur la page d'administration du site de l'association de Realitiie</h2>
-<h2>Choissiez quelle partie du site vous souhaitez éditer :</h2>
+<br/><br/>
 
-<form action="" class="admin"><input type="submit" value="Articles"/></form>
-<form action="" class="admin"><input type="submit" value="Projets"/></form>
-<form action="" class="admin"><input type="submit" value="Membres"/></form>
+<h2>Bienvenue sur la page d'administration du site de l'association de Realitiie</h2>
+<br/>
+<h2>Choissiez quelle partie du site vous souhaitez éditer :</h2>
+<br/><br/><br/>
+
+<form action=""><input type="submit" class="admin" value="Articles"/></form>
+<form action=""><input class="admin" type="submit" value="Projets"/></form>
+<form action=""><input class="admin" type="submit" value="Membres"/></form>
 
 </body>
