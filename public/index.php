a1580b0d75aca318e1529a2a0bad32a0d3750625
Ammar MOIZALY <ammar.moizaly@ensiie.fr>
2019-04-19 20:19:54 +0200

    integartion de index

--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
-require '../vendor/autoload.php';
-
-//postgres
-$dbName = getenv('DB_NAME');
-$dbUser = getenv('DB_USER');
-$dbPassword = getenv('DB_PASSWORD');
-$connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+require('../src/db.php');
+$co = new DataBase();
+$connection = $co->dbConnect();
 
 $userRepository = new \User\UserRepository($connection);
 $users = $userRepository->fetchAll();
