1149d0c9781e79c98686a63064bac0e0c0c8475f
Ammar MOIZALY <ammar.moizaly@ensiie.fr>
2019-04-19 20:35:51 +0200

    ajout de fichier

---
+++ public/loginView.php
@@ -0,0 +1,22 @@
+<?php $title = "Meetiie - Login"; 
+$css_link = "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/loginStyle.css\"/>";
+
+echo $css_link;
+?>
+
+
+<?php ob_start(); ?>
+<div class="connexion" onload="clear_form()">
+        <h1>Meetiie</h1>
+         <span id="conn">Connexion</span><br/>
+         <form action="checkPwd.php" method="POST">
+         <input type="email" name="email" placeholder="Adresse e-mail" size="35"required><br/>
+         <input type="password" name="password" placeholder="Mot de passe" size="35"required><br/>
+         <input type="submit" value="Login" onClick="clear_form()"><br/>
+         </form><br>
+         <a href="mdp_oublie.php">Mot de passe oublié ?</a><br/>
+         <a href="signup.php" title="Découvrez de nouveaux iiens">Créer un compte</a><br>
+         </div>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
---
+++ public/template.php
@@ -0,0 +1,15 @@
+<?php include_once 'loginView.php'; ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= $title ?></title>
+        
+    </head>
+        
+    <body>
+        <?= $content ?>
+    </body>
+</html>
