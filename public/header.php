87769c2988fd0e216c436a7075032361bfb932a1
hugo <hugo.sellambin1@gmail.com>
2019-04-21 18:27:53 +0200

    modifications

--- public/header.php
+++ public/header.php
@@ -24,6 +24,39 @@
                 </form>
             </nav>
 
+            <!--<nav>
+                <div class="topnav" id="myTopnav">
+                    <a href="index.php" class="active">Accueil</a>
+                    <div class="dropdown">
+                        <button class="dropbtn">Catégories 
+                        <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="#">Cat 1</a>
+                            <a href="#">Cat 2</a>
+                            <a href="#">Cat 3</a>
+                        </div>
+                    </div> 
+                    <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Connexion</button>
+                    <form action="" class="search-container">
+                        <input type="text" placeholder="Rechercher.." name="search">
+                        <button type="submit">OK</button>
+                    </form>
+                    <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="menuDeroulant()">&#9776;</a>
+                </div>
+            </nav> 
+
+            <script>
+                function menuDeroulant() {
+                var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } else {
+                x.className = "topnav";
+                }
+                }
+            </script> -->
+
             <div id="id01" class="modal">
         
         <form class="modal-content animate" action="/action_page.php">
