950893e16c3285e4698555d975e7889a84aaafc3
huang2018 <nina.huang@ensiie.fr>
2019-04-25 04:38:48 +0200

    removed responsive image switch due to flicker when loading & automatic scrolltop effect when opening toggle menu

--- public/responsive.js
+++ public/responsive.js
@@ -8,7 +8,8 @@ sidebarBody.style.display = 'none';
 sidebarBody.style.opacity = '0';
 
 //Switch menu display from "none" to "block" with fade in out animation on click. 
-document.getElementById('nav-toggle').onclick = function(){ 
+document.getElementById('nav-toggle').onclick = function(e){ 
+    e.preventDefault();
     menuBox = document.getElementById('sidebar-nav');
     if (menuBox.style.display == 'none'){
         $("#sidebar-nav").animate({opacity: 1});
@@ -21,21 +22,4 @@ document.getElementById('nav-toggle').onclick = function(){
         document.getElementById('nav-toggle').style.zIndex = "3";
     }
 }
-
-//Switch footer image responsively
-function footerImgSwitch(){
-    if (window.matchMedia("(min-width: 768px)").matches){
-        $("#contact-us-img").attr("src","img/contact_us.svg");
-    } else {
-        $("#contact-us-img").attr("src","img/questions.svg");
-    }
-}  
-
-$(document).ready(function(){
-    footerImgSwitch();
-})
-
-$(window).resize(function(){
-    footerImgSwitch();
-});
 
