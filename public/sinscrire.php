4e616ac290d4dd94d1119e6acc2d6f9da0452d70
hugo <hugo.sellambin1@gmail.com>
2019-05-01 22:34:24 +0200

    preview image profil

--- public/sinscrire.php
+++ public/sinscrire.php
@@ -28,20 +28,50 @@ require("header.php");
 	  	Pseudo <span class="red">*</span> : <br/><input type="text" size="20" maxlenght="30" name="id_user" placeholder="Pseudo" required> <br/>
 	  	Adresse mail <span class="red">*</span> : <br/><input type="text" name="email" placeholder="Email" required> <br/>
 	  	Mot de passe <span class="red">*</span> : <br/><input type="password" size="20" name="mdp" placeholder="Mot de passe" required> <br/>
-	  	Date de naissance : <br/><input type="date" name="bday"> <!-- ne fonctionne pas sur safari ou explorer... --> <br/>
+	  	Date de naissance : <br/><input type="date" name="bday"> <br/>
 	  	Ville <span class="red">*</span> : <br/><input type="text" name="ville" placeholder="Ville" required> <br/>
-		Photo de profil : <br/><input type="file" name="pp" accept="image/png, image/jpeg">
+		Photo de profil : <br/><input type="file" id="image_uploads" name="pp" accept="image/png, image/jpeg">
+		<div class="preview">
+    	
+  		</div>
 		<div class="flexbox_boutton">
 			<div class="bouton">
 				<input type="submit" value="Envoyer" name="inscription_bouton">
 			</div>
 			<div class="bouton">
-				<input type="reset" value="Annuler" name="reset_bouton">
+				<input type="reset" onclick="updateImageDisplay()" value="Annuler" name="reset_bouton">
 			</div>
 		  </div>
 	</form>
 	</div>
 </section>
+
+<script>
+	var input = document.querySelector('input[type=file]');
+	var preview = document.querySelector('.preview');
+
+	input.addEventListener('change', updateImageDisplay);
+
+	function updateImageDisplay() 
+	{
+		while(preview.firstChild) {
+    		preview.removeChild(preview.firstChild);
+		}
+		var curFiles = input.files;
+		if(curFiles.length === 0) {
+    		var para = document.createElement('p');
+    		para.textContent = 'Aucun fichier actuellement sélectionné';
+			preview.appendChild(para);
+		}
+		else {
+     		var para = document.createElement('p');
+        	var image = document.createElement('img');
+        	image.src = window.URL.createObjectURL(curFiles[0]);
+			preview.appendChild(image);
+			preview.appendChild(para);
+		}  
+	}
+</script>
 
 <?php
 require("aside.php");
