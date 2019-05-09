5b2f4f0b0ec6835b4d7425e9b717c391c044201c
hugo <hugo.sellambin1@gmail.com>
2019-05-09 21:01:28 +0200

    produit dans profil

--- public/pageProfil.php
+++ public/pageProfil.php
@@ -46,7 +46,58 @@ require("header.php");
             <p>E-mail : <?php echo $CurrUser[0]->getMail(); ?> </p>
             <p>Ville : <?php echo $CurrUser[0]->getLocation(); ?> </p>
             <p>Date de naissance : <?php echo date_format($CurrUser[0]->getBirthday(), 'd-m-Y'); ?> </p>
+            <button class="boutton" onclick="window.location.href='modifProfil.php'" style="width:auto;">Modifier mes infos</button>
         </div>
+    </div>
+
+    <h2 class="sous_titre">Mes annonces</h2>
+    <div class="produits">
+        
+        <a href="">
+        <div class="produit">
+            <div class="photo_prod">
+                <img class="preview" src="voiture.jpg" alt="photo du produit"/>
+            </div>
+            <div class="text_prod">
+                <p>
+                <span class="titre_prod">Très très très long Titre de l'annonce</span><br/><br/>
+                <span class="prix_prod">67 €</span><br/><br/>
+                <span class="details">Auto/Moto<br/>Evry</span>
+                </p>
+            </div>
+        </div>
+        </a>
+        <button class="boutton" onclick="" style="width:20%;">Supprimer</button>
+
+        <a href="">
+        <div class="produit">
+            <div class="photo_prod">
+                <img class="preview" src="hugo.JPG" alt="photo du produit"/>
+            </div>
+            <div class="text_prod">
+                <p>
+                <span class="titre_prod">Très très très long Titre de l'annonce</span><br/><br/>
+                <span class="prix_prod">67 €</span><br/><br/>
+                <span class="details">Auto/Moto<br/>Evry</span>
+                </p>
+            </div>
+        </div>
+        </a>
+
+        <a href="">
+        <div class="produit">
+            <div class="photo_prod">
+                <img class="preview" src="TTT_green.png" alt="photo du produit"/>
+            </div>
+            <div class="text_prod">
+                <p>
+                <span class="titre_prod">Très très très long Titre de l'annonce</span><br/><br/>
+                <span class="prix_prod">67 €</span><br/><br/>
+                <span class="details">Auto/Moto<br/>Evry</span>
+                </p>
+            </div>
+        </div>
+        </a>
     </div>
 </section>
 
