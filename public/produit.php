93c6c1b153841015198f22766431e84fa3d4fe0a
hugo <hugo.sellambin1@gmail.com>
2019-05-10 19:52:37 +0200

    description produit

--- public/produit.php
+++ public/produit.php
@@ -75,12 +75,62 @@ require("header.php");
 </div> 
 
 <!-- Description -->
+<div class="information">
 <div id="myDIV">
-  <button class="btnInfo activeInfo">Description</button>
-  <button class="btnInfo">Infos sur le vendeur</button>
+  <button class="btnInfo activeBtn">Description</button>
+  <button class="btnInfo">hugo91600</button>
   <button class="btnInfo">D'Autres annonces du vendeur</button>
 </div> 
 
+<div class ="infoBOX" id="infoBOX">
+
+  <div class="showInfo activeInfo">
+    <div class="description">
+      <h3>500 €</h3>
+      <h4>Ville</h4>
+      <p>Bonjour, je vends ma Peugeot 207<br/>
+      <br/>
+Marque : peugeot<br/>
+Modèle : 207<br/>
+Finition : Exécutive
+Motorisation : 1.4 HDI<br/>
+<br/>
+Contrôle technique avec contre visite rotule avant droite à changer avec ressort.<br/>
+<br/>
+Courroie de distribution faite à 159000km facture à l'appuie.<br/>
+Et révision à jour.<br/>
+parcours toute distance.<br/>
+<br/>
+Options :<br/>
+<br/>
+- Ordinateur de bord<br/>
+- Poste d'origine<br/>
+- Fermeture centralisée ( double des clés )<br/>
+- Commande au volant<br/>
+- Climatisation<br/>
+- Direction assistée<br/>
+- vitres électriques<br/>
+- Abs<br/>
+- Airbag<br/>
+
+Le véhicule est dans un bon état intérieur comme extérieur</p>
+    </div>
+  </div>
+
+  <div class="showInfo">
+    <div class="infoVendeur">
+      <p> Bonjour comment ca va</p>
+    </div>
+  </div>
+
+  <div class="showInfo">
+    <div class="otherProd">
+      <p>caca</p>
+    </div>
+  </div>
+
+</div>
+</div>
 </section>
 
 <script>
@@ -119,18 +169,32 @@ function showSlides(n) {
 <script>
 // Get the container element
 var btnContainer = document.getElementById("myDIV");
+var infContainer = document.getElementById("infoBOX");
 
 // Get all buttons with class="btn" inside the container
 var btns = btnContainer.getElementsByClassName("btnInfo");
+var infos = infContainer.getElementsByClassName("showInfo");
 
 // Loop through the buttons and add the active class to the current/clicked button
+for (var i = 1; i < infos.length; i++) {
+  infos[i].style.display = "none";
+}
+
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("activeInfo");
-    current[0].className = current[0].className.replace(" activeInfo", "");
-    this.className += " activeInfo";
+    var current = document.getElementsByClassName("activeBtn");
+    current[0].className = current[0].className.replace(" activeBtn", "");
+    this.className += " activeBtn";
+    for (var j=0; j<btns.length; j++){
+      if (btns[j].className === "btnInfo activeBtn") {
+        infos[j].style.display = "block";
+      }
+      else {
+        infos[j].style.display = "none";
+      }
+    }
   });
-} 
+}
 </script>
 
 <?php
