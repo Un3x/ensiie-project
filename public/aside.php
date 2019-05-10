ab9cca9ed08b29345a340b0eef176e67fb30d988
mattgoss <matthieu.gosset@yahoo.fr>
2019-05-10 03:03:54 +0200

    le widget insta etait lent
    je l'ai mis en commentaire

--- public/aside.php
+++ public/aside.php
@@ -1,6 +1,6 @@
     <aside>
     <a class="twitter-timeline" href="https://twitter.com/trouvetontruc?ref_src=twsrc%5Etfw">Tweets by trouvetontruc</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     <br/>
-<a href="https://instawidget.net/v/user/trouvetontruc" id="link-d74d7ca31692d4378dce0c01485ed1a975abd435b918181d8c389cfc899ab2a2">@trouvetontruc</a>
-<script src="https://instawidget.net/js/instawidget.js?u=d74d7ca31692d4378dce0c01485ed1a975abd435b918181d8c389cfc899ab2a2&width=300px"></script>
+<!-- <a href="https://instawidget.net/v/user/trouvetontruc" id="link-d74d7ca31692d4378dce0c01485ed1a975abd435b918181d8c389cfc899ab2a2">@trouvetontruc</a>
+<script src="https://instawidget.net/js/instawidget.js?u=d74d7ca31692d4378dce0c01485ed1a975abd435b918181d8c389cfc899ab2a2&width=300px"></script> -->
     </aside>
