b28d4686f0cd83abbf86a293b45914ac165da147
Elias Nacef <eliasnacef@gmail.com>
2019-05-11 14:16:12 +0200

    Inscription focntionne

--- ensiie-project-master/data/db.sql
+++ ensiie-project-master/data/db.sql
@@ -7,7 +7,7 @@ CREATE TABLE Utilisateur (
        nom VARCHAR(50) NOT NULL ,
        prenom VARCHAR(50) NOT NULL ,
        email VARCHAR(50) NOT NULL ,
-       password VARCHAR(50) UNIQUE NOT NULL ,
+       password VARCHAR(50) NOT NULL ,
        tel VARCHAR(10) ,
        genre CHAR(1),
        sport VARCHAR(50) NOT NULL );
--- ensiie-project-master/public/register_process.php
+++ ensiie-project-master/public/register_process.php
@@ -7,23 +7,36 @@
 	$dbUser = getenv('DB_USER');
 	$dbPassword = getenv('DB_PASSWORD');
 	$connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	$userRepository = new \User\UserRepository($connection);
 	$users = $userRepository->fetchAll();
 
 	if (isset($_POST['email']) && isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['tel']) && isset($_POST['sport']) && isset($_POST['genre']) && isset($_POST['pwd'])) {
 
-		$nid = "10";
-		$nnom = $_POST['nom'];
-		$nprenom = $_POST['prenom'];
-		$nemail = $_POST['email'];
-		$npassword = $_POST['pwd'];
-		$ntel = $_POST['tel'];
-		$ngenre = $_POST['genre'];
-		$nsport = $_POST['sport'];
-		$sql = 'INSERT INTO Utilisateur (id, nom, prenom, email, password, tel, genre, sport) VALUES (:nid, :nnom, :nprenom, :nemail, :npassword, :ntel, :ngenre, :nsport)';
-		$succes = $connection->prepare($sql)->execute(array('nid'=>$nid, 'nnom'=>$nnom, 'nprenom'=>$nprenom, 'nemail'=>$nemail, 'npassword'=>$npassword, 'ntel'=>$ntel, 'ngenre'=>$ngenre, 'nsport'=>$nsport));  /*TODO*/
+		
+		$sql = "INSERT INTO Utilisateur (id, nom, prenom, email, password, tel, genre, sport) VALUES (:nid, :nnom, :nprenom, :nemail, :npassword, :ntel, :ngenre, :nsport)";
+		$state = $connection->prepare($sql);
+		$state->bindParam(':nid', $nid);
+		$state->bindParam(':nnom', $nnom);
+		$state->bindParam(':nprenom', $nprenom);
+		$state->bindParam(':nemail', $nemail);
+		$state->bindParam(':npassword', $npassword);
+		$state->bindParam(':ntel', $ntel);
+		$state->bindParam(':ngenre', $ngenre);
+		$state->bindParam(':nsport', $nsport);
 
+		$nid = '10';
+		$nnom = ''.$_POST['nom'];
+		$nprenom = ''.$_POST['prenom'];
+		$nemail = ''.$_POST['email'];
+		$npassword = ''.$_POST['pwd'];
+		$ntel = ''.$_POST['tel'];
+		$ngenre = ''.$_POST['genre'];
+		$nsport = ''.$_POST['sport'];
+		$succes = $state->execute();
+
+		/*$state->execute(array('nid'=>$nid, 'nnom'=>$nnom, 'nprenom'=>$nprenom, 'nemail'=>$nemail, 'npassword'=>$npassword, 'ntel'=>$ntel, 'ngenre'=>$ngenre, 'nsport'=>$nsport));*/
 
 		if ($succes) {
 			header ('Location: index.php');
