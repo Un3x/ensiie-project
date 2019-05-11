f543668d1668e831439e518a79a1decef19381d1
thomas <guittonthomas41@gmail.com>
2019-05-11 16:38:13 +0200

    Application du calque image

--- public/app.js
+++ public/app.js
@@ -2,3 +2,9 @@
 var mymap = L.map('mapid').setView([48.623447, 2.425771], 14);
 
 //Création du calque du calque d'image
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 20,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
