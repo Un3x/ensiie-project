2faa6033aaeccf15a7d7a1e064cb407f5b139dd7
mattgoss <matthieu.gosset@yahoo.fr>
2019-05-12 00:31:30 +0200

    fichier erreurs quand ce sera stocké sur un serveur

---
+++ public/erreur.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+if (!isset($_SESSION['authent'])) {
+    $_SESSION['authent'] = 0;
+}
+require '../vendor/autoload.php';
+
+require 'connexion.php';
+
+require("header.php");
+?>
+
+<?php
+switch($_GET['erreur'])
+{
+   case '400':
+   echo 'Échec de l\'analyse HTTP.';
+   break;
+   case '401':
+   echo 'Le pseudo ou le mot de passe n\'est pas correct !';
+   break;
+   case '402':
+   echo 'Le client doit reformuler sa demande avec les bonnes données de paiement.';
+   break;
+   case '403':
+   echo 'Requête interdite !';
+   break;
+   case '404':
+   echo 'La page n\'existe pas ou plus !';
+   break;
+   case '405':
+   echo 'Méthode non autorisée.';
+   break;
+   case '500':
+   echo 'Erreur interne au serveur ou serveur saturé.';
+   break;
+   case '501':
+   echo 'Le serveur ne supporte pas le service demandé.';
+   break;
+   case '502':
+   echo 'Mauvaise passerelle.';
+   break;
+   case '503':
+   echo ' Service indisponible.';
+   break;
+   case '504':
+   echo 'Trop de temps à la réponse.';
+   break;
+   case '505':
+   echo 'Version HTTP non supportée.';
+   break;
+   default:
+   echo 'Erreur !';
+}
+?>
+
+<?php
+require("footer.php");
+?>
