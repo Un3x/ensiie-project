0caea13fb0a22f6fd5ea0cb450d4375c642030f1
Loïc Wikle DUBARD <loic97429@gmail.com>
2019-05-12 11:39:53 +0200

    ajout d'un evenement possible pour le président d'une asso

--- public/president.php
+++ public/president.php
@@ -23,6 +23,7 @@ $users = $userRepository->fetchAll();
 $user = $users[$_SESSION['login']];
 
 $assos_pres = $connection->query('select * from associations where president='.$user->getId())->fetchAll(\PDO::FETCH_OBJ);
+
 displayHeader();
 
 //un président doit pouvoir:
@@ -31,6 +32,11 @@ displayHeader();
 // - rajouter,modifier, enlever un évènement
 // - consulter, modifier, rajouter, enlever la participation d'un élève a un évènement
 // - 
+if (!empty($_POST['event_name']) && !empty($_POST['event_date']) && !empty($_POST['event_desc']) && !empty($_SESSION['association']) && !empty($_POST['coeff_event'])){
+	$date = new DateTime($_POST['event_date']);
+	$date= $date->format('Y-m-d');
+	$connection->query("insert into events(name,id_asso,coeff_event,date_ev,description_event) values ('".$_POST['event_name']."',".$_SESSION['association'].",".$_POST['coeff_event'].",'".$date."','".$_POST['event_desc']."')");
+}
 ?>
 
 <header class="header de page">
@@ -41,15 +47,74 @@ displayHeader();
   <label for="association">Gérer l'association : </label>
 	<select id="association" name="association">
 	<?php foreach ($assos_pres as $asso):?>
-	<option value="<?php echo $asso->id_asso ?>"><?php echo $asso->name ?></option>
+	<option value="<?php echo $asso->id_asso ?>" <?php if (!empty($_POST['association'])): if ($asso->id_asso == $_POST['association']): echo 'selected="selected"'; endif; endif; ?> ><?php echo $asso->name ?></option>
 	<?php endforeach; ?>
 	</select>
 	<input type="submit" name="submit" value="Gérer"/>
 </fieldset>
 </form>
 
-<?php if (!empty($_POST['association'])):?>
-c'pas vide !
+<?php if (!empty($_POST['association'])):
+$_SESSION['association']=$_POST['association'];
+$events=$connection->query('select * from events where id_asso='.$_POST['association'])->fetchAll(\PDO::FETCH_OBJ);
+?>
+
+
+<div class="gestion" id="gestion_evenements">
+
+	<table class="table table-bordered table-hover table-striped">
+		<caption>Récapitulatif des évènements</caption>
+		<tr>
+			<th>Evenement</th>
+			<th>Date</th>
+			<th>Description</th>
+			<th>Coeff</th>
+		</tr>
+		<?php foreach ($events as $event) : ?>
+		<tr>
+			<td><?php echo $event->name ?></td>
+			<td><?php echo $event->date_ev ?></td>
+			<td><?php echo $event->description_event ?></td>
+			<td><?php echo $event->coeff_event ?></td>
+		</tr>
+		<?php endforeach; ?>
+	</table>
+	
+
+	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+		<fieldset>
+		<legend>Créer un évènement</legend>
+		<p>
+		<label for="event_name">Nom : </label>
+		<input type="text" name="event_name" value=""/>
+		</p>
+
+		<p>
+		<label for="event_date">Date : </label>
+		<input type="date" name="event_date" value=""/>
+		</p>
+
+		<p>
+		<label for="event_desc">Description : </label>
+		<textarea class="desc" rows="5" cols="50" name="event_desc">Insérer la description</textarea>
+		</p>
+		<p>
+		<label for="coeff_event">coefficient : </label>
+		<input type="number" name="coeff_event" value="0" min="1" max="10"/>
+		</p>
+		<input type="submit" name="submit" value="Créer l'évènement"/>
+		</fieldset>
+	</form>
+</div>
+
+<div class="gestion" id="gestion_eleve">
+
+</div>
+
+<div class="gestion" id="gestion_transmission">
+
+</div>
+
 <?php endif; ?>
 
 </body>
