0889e9609dd52f62ce663ac3c3ed9030f46d8c27
Ansyth <tanguy.charles@ensiie.fr>
2019-05-12 12:02:11 +0200

    modif db

--- data/test.sql
+++ data/test.sql
@@ -5,16 +5,9 @@ INSERT INTO "User"(nom, prenom, pseudo, mdp, mail, nb_livres_empruntes, nb_livre
 
 
 
-INSERT INTO "Livre"(id_livre, titre, publication) VALUES('chocapic1', 'titre1', '2012-12-12');
-UPDATE "Livre" 
-SET emprunteur = '1'
-WHERE id_livre = 'chocapic1';
-UPDATE "User" 
-SET nb_livres_empruntes = nb_livres_empruntes + 99
-WHERE id_user = '1';
-UPDATE "Livre" 
-SET emprunteur = NULL
-WHERE id_livre = 'chocapic1';
+
+
+
 
 
 
