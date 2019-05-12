0aad86c1ed2ef3866f5da47bcc2befe8946d60e3
Tatoon <phulyjohn@gmail.com>
2019-05-12 17:18:53 +0200

    Indentation du code

--- ensiie-project-master/public/index.php
+++ ensiie-project-master/public/index.php
@@ -5,68 +5,68 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-<title>Challenge Centrale Evry</title>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="style.css" type="text/css">
+    <title>Challenge Centrale Evry</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css" type="text/css">
 
-<?php include ("header.php")?>
+    <?php include ("header.php")?>
 </head>
 <body>
-<div class="wrapper row1">
+    <div class="wrapper row1">
 
-</div>
-<!-- content -->
-<div class="wrapper row2">
-<div id="container" class="clear">
-<!-- Slider -->
-<section id="slider"><a href="#"><img src="images/cce.png" width="960" height="360" alt=""></a></section>
-<!-- main content -->
-<div id="homepage">
-<!-- Services -->
-<section id="services" class="clear">
-<article class="one_third">
-<figure><img src="images/sports.png" width="200" height="180" alt="">
-<figcaption>
-<h2>Sports</h2>
-<p>Venez découvrir tous les sports proposés, du plus connu au plus insolite !</p>
-<footer class="more"><a href="Sports.php">En savoir plus &raquo;</a></footer>
-</figcaption>
-</figure>
-</article>
-<article class="one_third">
-<figure><img src="images/ecoles.png" width="200" height="180" alt="">
-<figcaption>
-<h2>Ecoles</h2>
-<p>Presque toutes les écoles réservent leur spot au challenge. </p>
-</figcaption>
-</figure>
-</article>
-<article class="one_third lastbox">
-<figure><img src="images/particpants.png" width="200" height="180" alt="">
-<figcaption>
-<h2>Participants </h2>
-<p>Le Challenge accueille chaque année une multitude d'élèves de toute la France !</p>
-</figcaption>
-</figure>
-</article>
-</section>
+    </div>
+    <!-- content -->
+    <div class="wrapper row2">
+    <div id="container" class="clear">
+        <!-- Slider -->
+        <section id="slider"><a href="#"><img src="images/cce.png" width="960" height="360" alt=""></a></section>
+        <!-- main content -->
+        <div id="homepage">
+            <!-- Services -->
+            <section id="services" class="clear">
+                <article class="one_third">
+                    <figure><img src="images/sports.png" width="200" height="180" alt="">
+                        <figcaption>
+                            <h2>Sports</h2>
+                            <p>Venez découvrir tous les sports proposés, du plus connu au plus insolite !</p>
+                            <footer class="more"><a href="sports.php">En savoir plus &raquo;</a></footer>
+                        </figcaption>
+                    </figure>
+                </article>
+                <article class="one_third">
+                    <figure><img src="images/ecoles.png" width="200" height="180" alt="">
+                        <figcaption>
+                            <h2>Ecoles</h2>
+                            <p>Presque toutes les écoles réservent leur spot au challenge. </p>
+                        </figcaption>
+                    </figure>
+                </article>
+                <article class="one_third lastbox">
+                    <figure><img src="images/particpants.png" width="200" height="180" alt="">
+                        <figcaption>
+                            <h2>Participants </h2>
+                            <p>Le Challenge accueille chaque année une multitude d'élèves de toute la France !</p>
+                        </figcaption>
+                    </figure>
+                </article>
+            </section>
 
-<!-- Introduction -->
-<section id="intro" class="last clear">
-<article>
-<figure><img src="images/dd.jpg" width="450" height="250" alt="">
-<figcaption>
-<h2>Développement durable</h2>
-<p>Le Challenge Centrale Evry est un événement éco-responsable qui s’engage à réduire le plus possible les impacts environnementaux et sociaux négatifs tout en favorisant les retombées économiques locales.</p>
-<p>Cette manifestation s’engage également dans une démarche d’évaluation des impacts environnementaux, sociaux et économiques afin d’aboutir à une stratégie d’amélioration continue. Chaque participant s’engage aussi à respecter cette démarche.</p>
-</figcaption>
-</figure>
-</article>
-</section>
-</div>
-</div>
+            <!-- Introduction -->
+            <section id="intro" class="last clear">
+                <article>
+                    <figure><img src="images/dd.jpg" width="450" height="250" alt="">
+                        <figcaption>
+                            <h2>Développement durable</h2>
+                            <p>Le Challenge Centrale Evry est un événement éco-responsable qui s’engage à réduire le plus possible les impacts environnementaux et sociaux négatifs tout en favorisant les retombées économiques locales.</p>
+                            <p>Cette manifestation s’engage également dans une démarche d’évaluation des impacts environnementaux, sociaux et économiques afin d’aboutir à une stratégie d’amélioration continue. Chaque participant s’engage aussi à respecter cette démarche.</p>
+                        </figcaption>
+                    </figure>
+                </article>
+            </section>
+        </div>
+    </div>
 
-<?php include("footer.php") ?>
+    <?php include("footer.php") ?>
 
 </body>
 </html>
