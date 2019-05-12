7440deb29fe2a0313a69ebaed920c31b2e7c523d
Tatoon <phulyjohn@gmail.com>
2019-05-12 17:39:16 +0200

    indentation du code

--- ensiie-project-master/public/modify_account.php
+++ ensiie-project-master/public/modify_account.php
@@ -5,48 +5,48 @@
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-<head>
-<title>Challenge Centrale Evry</title>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="style.css" type="text/css">
+    <head>
+        <title>Challenge Centrale Evry</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css" type="text/css">
 
-<?php include ("header.php")?>
-</head>
+    <?php include ("header.php")?>
+    </head>
 
-<body>
-<div class="wrapper row2">
-<div id="container" class="clear">
+    <body>
+        <div class="wrapper row2">
+            <div id="container" class="clear">
 
 
-<div id="form_account" class="middle">
-<form action="modify_account_process.php" method="POST">
-<p>
-<br/><br/>
-<label for="actual_password">Password actuel :</label>
-<input type="password" id ="actual_password" name="pwd" required />
-</p>
-<p>
-<label for="password">Password :</label>
-<input type="password" id="password" name="pwd_modifier" required="" />
-</p>
-<p>
-<label for="tel">Téléphone :</label>
-<input type="text" id="tel" name="tel_modifier" required pattern="[0-9]{10}" />
-</p>
-<p>
-<input type="submit" id="btn" value="Valider" />
-</p>
-</form>
-<form action="index.php" method="POST">
-<p>
-<input type="submit" id="btn" value="Cancel" />
-</p>
-</form>
-</div>
-</div>
-</div>
-<?php include("footer.php"); ?>
-</div>
+                <div id="form_account" class="middle">
+                    <form action="modify_account_process.php" method="POST">
+                        <p>
+                        <br/><br/>
+                            <label for="actual_password">Password actuel :</label>
+                            <input type="password" id ="actual_password" name="pwd" required />
+                        </p>
+                        <p>
+                            <label for="password">Password :</label>
+                            <input type="password" id="password" name="pwd_modifier" required="" />
+                        </p>
+                        <p>
+                            <label for="tel">Téléphone :</label>
+                            <input type="text" id="tel" name="tel_modifier" required pattern="[0-9]{10}" />
+                        </p>
+                        <p>
+                            <input type="submit" id="btn" value="Valider" />
+                        </p>
+                    </form>
+                    <form action="index.php" method="POST">
+                        <p>
+                        <input type="submit" id="btn" value="Cancel" />
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <?php include("footer.php"); ?>
+        </div>
 </body>
 </html>
 
