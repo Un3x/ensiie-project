74e403afd76f5600f6651424b567279a8f97660f
Camille Balmond Leblanc <camillebalmondleblanc@gmail.com>
2019-05-12 10:44:15 +0000

    Update faq.php

--- ensiie-project-master/public/faq.php
+++ ensiie-project-master/public/faq.php
@@ -7,11 +7,34 @@
 <head>
 <title>Challenge Centrale Evry</title>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="style.css?version=1.94" type="text/css">
+<link rel="stylesheet" href="style.css?version=1.93" type="text/css">
 
 <?php include ("header.php");?>
 </head>
 <body>
+<div class="wrapper row2">
+<div id="container" class="clear">
+<div id="homepage">
+<section id="intro" class="last clear">
+
+<article>
+<figure>
+
+<h2>Comment puis-je participer ? </h2>
+<p>Pour participer au Challenge Centrale Evry, vous devez faire partie d’une école participante et contacter votre BDS qui aura les informations nécessaires.</p>
+<h2>Où vais-je dormir ?</h2>
+<p>Vous pourrez choisir lors de votre inscription si vous souhaitez qu’on s’occupe de ça pour vous. Toutes les filles dorment au chaud dans une chambre d’étudiant. Les garçons dorment en tentes sur le campus.</p>
+<h2> Que dois-je apporter ? </h2>
+<p>Vous devez apporter vos affaires de sport (maillots, chaussures, etc.). Vous devez aussi prévoir un duvet, un tapis de sol pour plus de confort ainsi que des vêtements chauds et imperméables car il arrive qu’il fasse froid pendant l’événement.</p>
+<h2>Que faire si mon école n’est pas encore une école participante ?</h2>
+<p>Nous aimerions accueillir toutes les personnes souhaitant participer au Challenge mais, pour des raisons de sécurité, nous ne pouvons pas davantage augmenter nos quotas. Il nous arrive toutefois d’inviter de nouvelles écoles pour participer au Challenge. Si votre établissement ne figure pas encore parmi les écoles participantes, votre BDS devra nous envoyer une demande qui nous convaincra de choisir votre école plutôt qu’une autre.</p>
+</figure>
+
+</article>
+</section>
+</div>
+</div>
+</div>
 
 <?php include("footer.php"); ?>
 
