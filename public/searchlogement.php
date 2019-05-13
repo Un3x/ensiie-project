dbd253fc8ae8fddf55875a6556043aaa340dc7c8
Pierre-Francois ALIAGA <pierre-francois.aliaga@pc130-04.pedago.ensiie.fr>
2019-05-13 20:18:58 +0200

    bdd

--- public/searchlogement.php
+++ public/searchlogement.php
@@ -123,6 +123,10 @@
 
     </form
 
-
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPassword = getenv('DB_PASSWORD');
+$connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+    
 </body>
 </html>
