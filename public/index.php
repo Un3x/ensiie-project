effb317c415d16c92d2370c00c74ab903b19f8ef
Leffe <antonino.le75@gmail.com>
2019-05-13 18:41:08 +0200

    push de menhir

--- public/index.php
+++ public/index.php
@@ -11,33 +11,39 @@ $userRepository = new \User\UserRepository($connection);
 $users = $userRepository->fetchAll();
 ?>
 
+<!DOCTYPE html>
 <html>
 <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title> Accueil </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../../public/res/css/accueil.css" />
+    <link rel="stylesheet" href="../../public/res/css/main.css" />
 </head>
 <body>
 
-<div class="container">
-    <h3><?php echo 'Hello world from Docker! php' . PHP_VERSION; ?></h3>
 
-    <table class="table table-bordered table-hover table-striped">
-        <thead style="font-weight: bold">
-            <td>#</td>
-            <td>Firstname</td>
-            <td>Lastname</td>
-            <td>Age</td>
-        </thead>
-        <?php /** @var \User\User $user */
-        foreach ($users as $user) : ?>
-            <tr>
-                <td><?php echo $user->getId() ?></td>
-                <td><?php echo $user->getFirstname() ?></td>
-                <td><?php echo $user->getLastname() ?></td>
-                <td><?php echo $user->getAge() ?> years</td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+
+<div id="colonne2">
+<a href="MonCompte.phtml">Se connecter</a> 
+<a href="Compte.phtml">S'inscrire</a>
+
 </div>
+
+
+<div id="colonne1">
+
+
+
+</div>
+
+<div id="centre">
+        <div id="recherche">
+            <a href="Recherche.phtml"> Je cherche une colocation </a>
+        </div>
+        <div id="createlogement">
+            <a href="Create_Logement.phtml"> Je propose une colocation </a>
+        </div>
+</div>
+
 </body>
 </html>
