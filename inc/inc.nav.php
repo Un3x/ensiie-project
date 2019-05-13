633bcd23c6e1a9490157b74908f39120959f8d04
acherouf2018 <yacine.acheroufkebir@gmail.com>
2019-05-13 19:54:56 +0200

    boutton de deco

--- inc/inc.nav.php
+++ inc/inc.nav.php
@@ -3,15 +3,17 @@ function navAccueil()
 {
 	echo "\t<nav>\n";
 	$date = date( "d/m/Y" );
-	echo "<p>Date : $date</p>";
+	
 	if( isset( $_SESSION['pseudo'] ) )
 	{
+		echo "\t<p><a href=\"../public/deconnexion.php\"><img class=\"boutton_co\" src=\"../img/bouton_deco.png\"></button></a></p>\n";
 		echo "\t<p> Bonjour ".$_SESSION['pseudo']."</p>\n";
-		echo "\t<a href=\"../public/deconnexion.php\"><button type=\"button\" class=\"bDecoCo\">Déconnexion</button></a>\n";
+		echo "<p>Date : $date</p>";
 	}
 	else
 	{
 		echo "\t<a href=\"../public/connexion.php\"><button type=\"button\" class=\"bDecoCo\">Connexion</button></a>\n";
+		echo "<p>Date : $date</p>";
 	}
 	echo "\t</nav>\n";
 	echo "<article>\n";
