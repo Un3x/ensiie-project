a1907b388a4eeb756417c2a25f5c422ea74d4c64
song <songmaxime@gmail.com>
2019-05-13 16:37:45 +0200

    modifié :         template.php

--- src/View/template.php
+++ src/View/template.php
@@ -20,11 +20,7 @@
     <body>
         <nav class="row notmain">
             <ul>
-                <li class="col-md-7">
-                    <a href='' onclick="document.cookie = 'lang=fr'" class="whiteStyle">Fr</a>/
-                    <a href='' onclick="document.cookie = 'lang=elf'" class="whiteStyle">Elf</a>
-                </li>
-                <li class="col-md-1">  
+                <li class="col-md-7">  
                     <a href="/" class="whiteStyle">Uber Licorne</a>
                 </li>
                 <li class="col-md-1">  
@@ -42,31 +38,23 @@
                     {
                 ?>
                         <li>
-<<<<<<< HEAD
                             <a href="index.php?action=deconnexion" class="whiteStyle">Deconnexion</a>
                         </li>
                 <?php
                     }       
-=======
-                            <a href="/index.php?action=deconnexion" class="whiteStyle">Deconnexion</a>
-                        </li>
-                <?php
-                    }   
->>>>>>> 263f37b19b4e67b0ef5f2a08001962f082b1fa7f
                     else
                     { 
                 ?>
                         <li class="col-md-1"> 
-<<<<<<< HEAD
                             <a href="index.php?action=connexion" class="whiteStyle" >Connexion</a> / 
                             <a href="index.php?action=choixInscription" class="whiteStyle">Inscription</a> 
-=======
-                            <a href="/index.php?action=connexion" class="whiteStyle" >Connexion</a> / 
-                            <a href="/index.php?action=choixInscription" class="whiteStyle">Inscription</a> 
->>>>>>> 263f37b19b4e67b0ef5f2a08001962f082b1fa7f
                         </li>
                 <?php }
                 ?>
+                <li class="col-md-1">
+                    <a href='' onclick="document.cookie = 'lang=fr'" class="whiteStyle">Fr</a>/
+                    <a href='' onclick="document.cookie = 'lang=elf'" class="whiteStyle">Elf</a>
+                </li>
             </ul>
         </nav>
 
@@ -81,23 +69,16 @@
 
 
         <footer class="row notmain">
-            <ul class="col-xs-6">
-<<<<<<< HEAD
+            <ul class="col-xs-3"></ul>
+            <ul class="col-xs-3">
                 <li>A propos de nous</li>
                 <li>Contactez-nous</li>
              </ul>
-            <ul class="col-xs-6"> 
+            <ul class="col-xs-3"> 
                 <li>Nos conditions d'utilisations</li>      
                 <li>Cliquez ici pour vendre votre âme </li>
-=======
-                <li><a href="/aPropos" class="whiteStyle">A propos de nous</a></li>
-                <li><a href="/contactezNous" class="whiteStyle">Contactez-nous</a></li>
-             </ul>
-            <ul class="col-xs-6"> 
-                <li><a href="/conditionUtilisation" class="whiteStyle">Nos conditions d'utilisations</a></li>      
-                <li><a href="/venteAme" class="whiteStyle">Cliquez ici pour vendre votre âme</a></li>
->>>>>>> 263f37b19b4e67b0ef5f2a08001962f082b1fa7f
             </ul>
+            <ul class="col-xs-3"></ul>
         </footer>
     </body>
 
