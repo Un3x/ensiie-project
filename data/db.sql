9d8957d8bda2c966aa94377d81623844853c3d90
Pierre-Francois ALIAGA <pierre-francois.aliaga@pc130-04.pedago.ensiie.fr>
2019-05-13 17:42:52 +0200

    maj bdd

--- data/db.sql
+++ data/db.sql
@@ -42,11 +42,11 @@ PRIMARY KEY (idUser,idLogement)
 
 INSERT INTO "user"(pseudo, firstname, lastname, birthday, mdp) VALUES ('jd','John', 'Doe', '1967-11-22','john');
 INSERT INTO "user"(pseudo, firstname, lastname, birthday, mdp) VALUES ('ya','Yvette', 'Angel', '1932-01-24','yvette');
-INSERT INTO "user"(pseudo, firstname, lastname, birthday, mdp) VALUES ('Amelia', 'Waters', '1981-12-01','amelia');
+INSERT INTO "user"(pseudo, firstname, lastname, birthday, mdp) VALUES ('aw', 'Amelia', 'Waters', '1981-12-01','amelia');
 
 
-INSERT INTO "logement"(idUser, departement, ville, nb_places_libres, prix) VALUES (1, 91 , 'Evry', 2, 400);
-INSERT INTO "logement"(idUser, departement, ville, nb_places_libres, prix) VALUES (2, 91 , 'Evry', 1, 500);
-INSERT INTO "logement"(idUser, departement, ville, nb_places_libres, prix) VALUES (3, 13 , 'Marseille', 2, 400);
+INSERT INTO "logement"(idUser, departement, ville, nb_places_libres, prix) VALUES (0, 91 , 'Evry', 2, 400);
+INSERT INTO "logement"(idUser, departement, ville, nb_places_libres, prix) VALUES (1, 91 , 'Evry', 1, 500);
+INSERT INTO "logement"(idUser, departement, ville, nb_places_libres, prix) VALUES (2, 13 , 'Marseille', 2, 400);
 
 INSERT INTO "favoris"(idUser, idLogement) VALUES (1,2);
