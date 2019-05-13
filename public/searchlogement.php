9a052ad2f242b96c70a6e754bd2d46ecf49e4be3
Leffe <antonino.le75@gmail.com>
2019-05-13 20:07:16 +0200

    changement departement

--- public/searchlogement.php
+++ public/searchlogement.php
@@ -9,107 +9,106 @@
     <form action="recherche.php" method = "POST">
         <p> Département </p>
         <select name="département">
-            <option>01   Ain </option> 
-            <option>02   Aisne </option>
-            <option>03   Allier </option>
-            <option>04   Alpes-de-Haute-Provence</option>
-            <option>05   Hautes-Alpes </option>
-            <option>06   Alpes-Maritimes </option>
-            <option>07   Ardèche </option>
-            <option>08   Ardennes</option>
-            <option>09   Ariège</option>
-            <option>10   Aube </option>
-            <option>11   Aude </option>
-            <option>12   Aveyron</option>
-            <option>13   Bouches-du-Rhône </option>
-            <option>14   Calvados </option>
-            <option>15   Cantal </option>
-            <option>16   Charente </option>
-            <option>17   Charente-Maritime</option>
-            <option>18   Cher</option>
-            <option>19   Corrèze</option>
-            <option>21   Côte-d'Or</option>
-            <option>22   Côtes d'Armor </option>
-            <option>23   Creuse </option>
-            <option>24   Dordogne </option>
-            <option>25   Doubs </option>
-            <option>26   Drôme </option>
-            <option>27   Eure </option>
-            <option>28   Eure-et-Loir 	</option>
-            <option>29   Finistère </option>
-            <option>30   Gars </option>
-            <option>31   Haute-Garonne </option>
-            <option>32   Gers </option>
-            <option>33   Gironde </option>
-            <option>34   Hérault</option>
-            <option>35 	 Ille-et-Vilaine</option>
-            <option>36   Indre </option>
-            <option>37   Indre-et-Loire </option>
-            <option>38   Isère</option>
-            <option>39   Jura</option>
-            <option>40   Landes</option>
-            <option>41   Loir-et-Cher</option>
-            <option>42   Loire</option>
-            <option>43   Haute-Loire</option>
-            <option>44   Loire-Atlantique</option>
-            <option>45   Loiret</option>
-            <option>46   Lot</option>
-            <option>47   Lot-et-Garonne</option>
-            <option>48   Lozère</option>
-            <option>49   Maine-et-Loire</option>
-            <option>50   Manche</option>
-            <option>51   Marne</option>
-            <option>52   Haute-Marne</option>
-            <option>53   Mayenne</option>
-            <option>54   Meurthe-et-Moselle</option>
-            <option>55 	 Meuse</option>
-            <option>56   Morbihan</option>
-            <option>57   Moselle</option>
-            <option>58   Nièvre</option>
-            <option>59   Nord</option>
-            <option>60   Oise</option>
-            <option>61   Orne</option>
-            <option>62   Pas-de-Calais</option>
-            <option>63   Puy-de-Dôme</option>
-            <option>64   Pyrénées-Atlantiques</option>	
-            <option>65 	 Hautes-Pyrénées</option>
-            <option>66   Pyrénées-Orientales </option>	
-            <option>67   Bas-Rhin</option>
-            <option>68   Haut-Rhin</option>
-            <option>69   Rhône</option>
-            <option>70   Haute-Saône</option>
-            <option>71 	 Saône-et-Loire</option>
-            <option>72   Sarthe </option>
-            <option>73   Savoie </option>
-            <option>74   Haute-Savoie</option>
-            <option>75   Paris</option>
-            <option>76   Seine-Maritime</option>
-            <option>77   Seine-et-Marne</option>
-            <option>78   Yvelines</option>
-            <option>79   Deux-Sèvres</option>
-            <option>80   Somme</option>
-            <option>81   Tarn</option>
-            <option>82   Tarn-et-Garonne</option>
-            <option>83   Var</option>
-            <option>84   Vaucluse</option>
-            <option>85   Vandée</option>
-            <option>86   Vienne</option>
-            <option>87 	 Haute-Vienne</option>
-            <option>88 	 Vosges</option>
-            <option>89 	 Yonne</option>
-            <option>90 	 Territoire de Belfort</option>
-            <option>91 	 Essonne</option>
-            <option>92   Hauts-de-Seine</option>
-            <option>93   Seine-St-Denis</option>
-            <option>94   Val-de-Marne</option>
-            <option>95   Val-D-Oise</option>
-            <option>2A   Corse-du-Sud</option>
-            <option>2B   Haute-Corse</option>
-            <option>971  Guadeloupe</option>
-            <option>972  Martinique</option>
-            <option>973  Guyane</option>
-            <option>974  La Réunion</option>
-            <option>976  Mayotte</option>
+            <option>01</option>
+            <option>02</option>
+            <option>03</option>
+            <option>04</option>
+            <option>05</option>
+            <option>06</option>
+            <option>07</option>
+            <option>08</option>
+            <option>09</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
+            <option>13</option>
+            <option>14</option>
+            <option>15</option>
+            <option>16</option>
+            <option>17</option>
+            <option>18</option>
+            <option>19</option>
+            <option>20</option>
+            <option>21</option>
+            <option>22</option>
+            <option>23</option>
+            <option>24</option>
+            <option>25</option>
+            <option>26</option>
+            <option>27</option>
+            <option>28</option>
+            <option>29</option>
+            <option>30</option>
+            <option>31</option>
+            <option>32</option>
+            <option>33</option>
+            <option>34</option>
+            <option>35</option>
+            <option>36</option>
+            <option>37</option>
+            <option>38</option>
+            <option>39</option>
+            <option>40</option>
+            <option>41</option>
+            <option>42</option>
+            <option>43</option>
+            <option>44</option>
+            <option>45</option>
+            <option>46</option>
+            <option>47</option>
+            <option>48</option>
+            <option>49</option>
+            <option>50</option>
+            <option>51</option>
+            <option>52</option>
+            <option>53</option>
+            <option>54</option>
+            <option>55</option>
+            <option>56</option>
+            <option>57</option>
+            <option>58</option>
+            <option>59</option>
+            <option>60</option>
+            <option>61</option>
+            <option>62</option>
+            <option>63</option>
+            <option>64</option>	
+            <option>65</option>
+            <option>66</option>	
+            <option>67</option>
+            <option>68</option>
+            <option>69</option>
+            <option>70</option>
+            <option>71</option>
+            <option>72</option>
+            <option>73</option>
+            <option>74</option>
+            <option>75</option>
+            <option>76</option>
+            <option>77</option>
+            <option>78</option>
+            <option>79</option>
+            <option>80</option>
+            <option>81</option>
+            <option>82</option>
+            <option>83</option>
+            <option>84</option>
+            <option>85</option>
+            <option>86</option>
+            <option>87</option>
+            <option>88</option>
+            <option>89</option>
+            <option>90</option>
+            <option>91</option>
+            <option>92</option>
+            <option>93</option>
+            <option>94</option>
+            <option>95</option>
+            <option>971</option>
+            <option>972</option>
+            <option>973</option>
+            <option>974</option>
+            <option>976</option>
         </select>
 
         <p> Ville </p>
