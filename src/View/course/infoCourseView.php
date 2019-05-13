c69fee13373747516c7677f48298b62934af2d95
Toro <corentinglh@gmail.com>
2019-05-13 20:04:02 +0200

    jesaispasquoimettre

--- src/View/course/infoCourseView.php
+++ src/View/course/infoCourseView.php
@@ -4,11 +4,11 @@
 
 <div>
 	nom : <a href='/userInfos/<?= $userType=="Vendor" ? "Client" : "Vendor" ?>/<?=$id?>'> <?=$name ?> </a> <br/>
-	prix : <?=$price ?> €<br/>
+	prix : <?=$price ?> <img src="/img/Pokedollar.png" alt="Pokedollar"><br/>
 	lieu de départ : <?=$departureName ?> <br/>
 	lieu d'arrivée : <?=$arrivalName ?> <br/>
-	distance : <?=$distance ?> <br/>
-	durée : <?=$duration ?> <br/>
+	distance : <?=$distance ?>·10<sup>38</sup> l<sub>p</sub><br/>
+	durée : <?=$duration ?>·10<sup>47</sup> t<sub>p</sub><br/>
 </div>
 
 <div id='mapId' style='width: 600px; height: 400px;'></div>
