440aae0c9db7b2410c4848269128b6dcdee33f57
Pierre Poiblaud <pierrepoiblaud@MacBook-Pro-de-Pierre.local>
2019-05-13 21:51:42 +0200

    css

--- public/connexion.php
+++ public/connexion.php
@@ -1,18 +1,23 @@
 <link rel="stylesheet" href="res/css/connexion.css" />
+<?php include_once("navbar.phtml"); ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title> S'inscrire </title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../../public/res/css/main.css" />
+    <link rel="stylesheet" href="/res/css/form.css" />
 </head>
-<?php include_once("navbar.phtml"); ?>
+
 <body>
-    <form action="connexion.php" method = "POST">
-        <p> Email </p>
-        <input type="text" name="email">
-        <p> Mot de passe </p>
-        <input type="text" name="mdp">
+    <div class="login">
+        <h1>Connexion</h1>
+        <form action="connexion.php" method = "POST">
+           <p> Email </p>
+          <input type="text" name="email">
+            <p> Mot de passe </p>
+            <input type="password" name="mdp">
+            <p><input type="submit"></p>
         </form>
+    </div>
 </body>
