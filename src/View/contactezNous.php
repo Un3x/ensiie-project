59a852a9594f12c89cb7e6ca50efb0d333f63d51
alexandre garand <aran.alexandre@gmail.com>
2019-05-13 07:55:26 +0000

    Upload New File

---
+++ src/View/contactezNous.php
@@ -0,0 +1,12 @@
+<?php $title = "Contactez nous" ?>
+
+<?php ob_start(); ?>
+
+<section>
+    <p> Pensez très fort à moi et dites trois fois mon nom et j'arrive
+     </p>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require "template.php"; ?>
