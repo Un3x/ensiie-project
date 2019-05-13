8235a8f838458a29d5556190d1f26cda6e69f9c0
alexandre garand <aran.alexandre@gmail.com>
2019-05-13 08:35:55 +0000

    Update contactezNousView.php

--- src/View/contactezNousView.php
+++ src/View/contactezNousView.php
@@ -3,8 +3,18 @@
 <?php ob_start(); ?>
 
 <section>
-    <p> Pensez très fort à moi et dites trois fois mon nom et j'arrive
-     </p>
+    <p> Pensez très fort à moi et dites trois fois mon nom et j'arrive ou envoyez un mail:
+    </p>
+    <form method="POST" action="index.php?action=contactezNousControleurs">
+          <label> sujet: </label>
+
+           <input type="text" name="sujet"/>
+               <br/>
+               <label> Mot de passe : </label>
+           <input type="text" name="corp"/>
+                <br/>
+       <input type="submit" value="envoyez"/>
+    </form>
 </section>
 
 <?php $content = ob_get_clean(); ?>
