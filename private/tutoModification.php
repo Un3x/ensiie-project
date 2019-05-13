b89a9ba9475695aaf9a399108c7e765a45af701c
Matteo BRANDI <matteo.brandi@ensiie.fr>
2019-05-14 00:39:04 +0200

    tuto ajout/modif/suppression

--- private/tutoModification.php
+++ private/tutoModification.php
@@ -5,13 +5,12 @@
 <body>
 
 <?php
-require '../src/Article/Article.php';
-require '../src/Article/ArticleRepository.php';
-require '../src/Membre/Membre.php';
-require '../src/Membre/MembreRepository.php';
+$id_page="tuto";
+require '../src/Tuto/Tuto.php';
+require '../src/Tuto/TutoRepository.php';
 require( "../inc/inc.default.php" );
 require( "../inc/inc.nav.php" );
-entete( "Accueil" );
+entete( "Accueil", $id_page);
 navAccueil();
 
 if(!isset($_SESSION['pseudo'])){ //Si pas connecté, renvoie vers la page de connexion
@@ -19,92 +18,62 @@ if(!isset($_SESSION['pseudo'])){ //Si pas connecté, renvoie vers la page de con
     exit();
 }
 
-$dbName = 'realitiie';
-$dbUser = 'postgres';
-$dbPassword = 'postgres';
-$connection = new PDO("pgsql:host=localhost user=$dbUser dbname=$dbName password=$dbPassword");
-
-$articleRepository = new \Article\ArticleRepository($connection);
+$tutoRepository = new \Tuto\TutoRepository($connection);
 
 
-if(!isset($_GET['id'])){ //Si aucun article selectionné, renvoie vers la page de d'administration des articles
-    header("location: article.php");
+if(!isset($_GET['id'])){ //Si aucun tuto selectionné, renvoie vers la page de d'administration des tutos
+    header("location: tuto.php");
 }
 
-$article = $articleRepository->getArticle($_GET['id']);
+$tuto = $tutoRepository->getTuto($_GET['id']);
 
-if($article == NULL){ //Si article introuvable, renvoie vers la page de d'administration des articles
-    echo '<h4>Erreur: l\'article n°'.$_GET['id'].' est introuvable!</h4>';
-    echo '<h4>Redirection vers la liste des articles...</h4>';
-    header( "refresh:3;url=article.php" );
+if($tuto == NULL){ //Si tuto introuvable, renvoie vers la page de d'administration des tutos
+    echo '<h4>Erreur: le tuto n°'.$_GET['id'].' est introuvable!</h4>';
+    echo '<h4>Redirection vers la liste des tutos...</h4>';
+    header( "refresh:3;url=tuto.php" );
 
-}else if($_SESSION['role'] != 'a' && $article->getAuteur()->getId() != $_SESSION['id']){ //Si pas administrateur, renvoie vers d'administration
-        echo '<h4>Erreur: Vous n\'avez pas la permission de modifier cette article car vous n\'en êtes pas l\'auteur</h4>';
-        echo '<h4>Redirection vers la page d\'administration...</h4>';
-        header( "refresh:3;url=admin.php" );
-}else if(isset($_POST['modification'])){ //Si article est modfifié, modification de la bdd puis renvoie vers la page de d'administration des articles
-    if(!isset($_POST['auteur'])){
-        $_POST['auteur'] = $_SESSION['id'];
-    }
+}else if(isset($_POST['modification'])){ //Si tuto est modfifié, modification de la bdd puis renvoie vers la page de d'administration des tutos
     
-    $status = $articleRepository->setArticle($_GET['id'], $_POST['titre'], $_POST['texte'], $_POST['auteur'], $_POST['date']);
+    $status = $tutoRepository->setTuto($_GET['id'], $_POST['titre'], $_POST['texte'], $_POST['pdf']);
     
     if($status){
-        echo '<h4>L\'article n°'.$_GET['id'].' a bien été modifié</h4>';
+        echo '<h4>Le tuto n°'.$_GET['id'].' a bien été modifié</h4>';
     }else{
-        echo '<h4>Erreur: la modification de l\'article n°'.$_GET['id'].' a échoué!</h4>';
+        echo '<h4>Erreur: la modification du tuto n°'.$_GET['id'].' a échoué!</h4>';
     }
     
-    echo '<h4>Redirection vers la liste des articles...</h4>';
-    header( "refresh:3;url=article.php" );
-}else if(isset($_POST['supression'])){ //Si article est supprimé, modification de la bdd puis renvoie vers la page de d'administration des articles
+    echo '<h4>Redirection vers la liste des tutos...</h4>';
+    header( "refresh:3;url=tuto.php" );
+}else if(isset($_POST['supression'])){ //Si tuto est supprimé, modification de la bdd puis renvoie vers la page de d'administration des tutos
     
-    $articleRepository->deleteAllMedia($_GET['id']);
-    $status = $articleRepository->deleteArticle($_GET['id']);
+    $status = $tutoRepository->deleteTuto($_GET['id']);
     
     if($status){
-        echo '<h4>L\'article n°'.$_GET['id'].' a bien été supprimé</h4>';
+        echo '<h4>Le tuto n°'.$_GET['id'].' a bien été supprimé</h4>';
     }else{
-        echo '<h4>Erreur: la supression de l\'article n°'.$_GET['id'].' a échoué!</h4>';
+        echo '<h4>Erreur: la supression du tuto n°'.$_GET['id'].' a échoué!</h4>';
     }
     
-    echo '<h4>Redirection vers la liste des articles...</h4>';
-    header( "refresh:3;url=article.php" );
+    echo '<h4>Redirection vers la liste des tutos...</h4>';
+    header( "refresh:3;url=tuto.php" );
 }else{
-    
-    $membreRepository = new \Membre\MembreRepository($connection);
-    $membres = $membreRepository->fetchAll();
 
-    echo '<h1>Modification de l\'article n°'.$article->getId().'</h1>';
+    echo '<h1>Modification du tutoriel n°'.$tuto->getId().'</h1>';
                 
     ?>
     <div class="modifContainer">
         <form action="" method="POST">
-        	<label>Titre : </label><input name="titre" type="text" value="<?php echo $article->getTitre() ?>" required/>
+        	<label>Titre : </label><input name="titre" type="text" required value="<?php echo $tuto->getTitre() ?>"/>
         	<br/>
-        	<label>Texte : </label><textarea name="texte" rows="5" cols="40" required><?php echo $article->getTexte() ?></textarea>
+        	<label>Texte : </label><textarea name="texte" rows="5" cols="40" required><?php echo $tuto->getTexte() ?></textarea>
         	<br/>
-        	<?php if($_SESSION['role'] == 'a'){?>
-            	<label>Auteur : </label>
-            	<select name="auteur" required>
-            		<?php
-            		foreach ($membres as $membre){
-            		    if($membre->getId() == $article->getAuteur()->getId()){ //Selectionne l'auteur de l'article
-                            echo '<option value="'.$membre->getId().'" selected>'.$membre->getSurnom().'</option>';
-            		    }else{
-            		        echo '<option value="'.$membre->getId().'">'.$membre->getSurnom().'</option>';
-            		    }
-            		}
-            		?>
-            	</select>
-            	<br/>
-            <?php } ?>
-        	<label>Date de publication : </label><input name="date" type="date" value="<?php echo $article->getDate()->format('Y-m-d') ?>" required/>
+        	<label>Lien du PDF : </label><input name="pdf" type="text" value="<?php echo $tuto->getPdf() ?>" required/>
+        	
         	<input type="submit" name="modification" value="Envoyer"/>
         </form>
     </div>
     
-    <form action="" method="POST"><input name ="supression" type="submit" class="moins" value="Supprimer l'article"/></form>
+    <form action="" method="POST"><input name ="supression" type="submit" class="moins" value="Supprimer l'tuto"/></form>
  
 <?php
 }
