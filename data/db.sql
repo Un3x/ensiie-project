3ed1bd152678691fd8c73c3336f4bff56bb097de
Pierre-Francois ALIAGA <pierre-francois.aliaga@pc130-04.pedago.ensiie.fr>
2019-05-13 17:16:20 +0200

    maj bdd

--- data/db.sql
+++ data/db.sql
@@ -4,24 +4,24 @@ CREATE TABLE "user" (
 	firstname VARCHAR NOT NULL ,
     lastname VARCHAR NOT NULL ,
     birthday date,
-    mdp VARCHAR NOT NULL,
+    mdp VARCHAR NOT NULL
 );
 
 CREATE TABLE "logement" (
 idLogement SERIAL PRIMARY KEY,
-idUser INT UNSIGNED,
-departement INT UNSIGNED,
+idUser INT,
+departement INT,
 ville VARCHAR NOT NULL,
 nb_places_libres INT,
-prix INT UNSIGNED,
+prix INT,
 CONSTRAINT fk_user
 	   FOREIGN KEY (idUser)
 	   REFERENCES user(idUser)
 );
 
 CREATE TABLE "favoris" (
-idUser INT UNSIGNED,
-idLogement INT UNSIGNED,
+idUser INT,
+idLogement INT,
 CONSTRAINT fk_user2
 	   FOREIGN KEY (idUser)
 	   REFERENCES user(idUser),
@@ -54,3 +54,69 @@ INSERT INTO "logement"(idUser, departement, ville, nb_places_libres, prix) VALUE
 
 INSERT INTO "favoris"(idUser, idLogement) VALUES (1,2);
 
+
+
+/** DEBUT DES PERMISSIONS */
+
+/** les roles possibles */
+CREATE TABLE "role" (
+	id		SERIAL	PRIMARY KEY,
+	name	VARCHAR	NOT NULL UNIQUE
+);
+
+/** les roles associés à un utilisateur */
+CREATE TABLE "utilisateur_role" (
+	utilisateur_id	INTEGER	NOT NULL,
+	FOREIGN KEY (utilisateur_id) REFERENCES user(idUser),
+	
+	role_id			INTEGER	DEFAULT 1,
+	FOREIGN KEY (role_id) REFERENCES role(id),
+	
+	PRIMARY KEY (utilisateur_id, role_id)
+);
+
+/** les permissions */
+CREATE TABLE "permission" (
+	id 			SERIAL			PRIMARY KEY,
+	name		VARCHAR(32)		NOT NULL UNIQUE
+);
+
+/** les permissions associés à un role */
+CREATE TABLE "role_permission" (
+  role_id		INTEGER	NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+
+  permission_id	INTEGER	NOT NULL,
+  FOREIGN KEY (permission_id) REFERENCES permission(id),
+  
+  PRIMARY KEY (role_id, permission_id)
+);
+
+/** les roles */
+INSERT INTO role (name) VALUES ('utilisateur');			/* 1 */
+INSERT INTO role (name) VALUES ('moderateur');		/* 2 */
+INSERT INTO role (name) VALUES ('administrateur');	/* 3 */
+
+/** les permissions */
+INSERT INTO permission (name) VALUES ('create_logement');		/* 1 */
+INSERT INTO permission (name) VALUES ('modif_logement');	/* 2 */
+INSERT INTO permission (name) VALUES ('create_role');		/* 3 */
+INSERT INTO permission (name) VALUES ('delete_role');		/* 4 */
+INSERT INTO permission (name) VALUES ('assign_role');		/* 5 */
+INSERT INTO permission (name) VALUES ('unassign_role');		/* 6 */
+INSERT INTO permission (name) VALUES ('add_permission');	/* 7 */
+INSERT INTO permission (name) VALUES ('remove_permission');	/* 8 */
+
+/* utilisateur */
+INSERT INTO role_permission (role_id, permission_id) VALUES (1, 1); /* create_logement */
+INSERT INTO role_permission (role_id, permission_id) VALUES (1, 1); /* create_logement */
+
+/* modérateur */
+INSERT INTO role_permission (role_id, permission_id) VALUES (2, 1); /* create_logement */
+INSERT INTO role_permission (role_id, permission_id) VALUES (2, 2); /* modif_logement */
+
+/* administrateur : toutes les permissions */
+INSERT INTO role_permission (role_id, permission_id) SELECT 3, id FROM permission ;
+
+
+/** FIN DES PERMISSIONS */
