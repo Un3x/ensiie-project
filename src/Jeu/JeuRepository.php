f794be26eb9d1c0f42e992fc96177bcfae5f4fc5
brandi2018 <matteo.brandi.pro@gmail.com>
2019-05-14 09:33:15 +0200

    ajout image projet

--- src/Jeu/JeuRepository.php
+++ src/Jeu/JeuRepository.php
@@ -87,4 +87,48 @@ class JeuRepository
         $status = $req->execute(array($titre, $git, $telechargement, $description));
         return $status;
     }
+	
+	public function deleteAllMedia($id){
+        $rows = $this->connection->query('SELECT lien
+                                          FROM media
+                                          WHERE id_jeu = '.$id)->fetchAll(\PDO::FETCH_OBJ);
+        foreach ($rows as $row) {
+            if (file_exists($row->lien)) {
+                unlink($row->lien);
+            }
+        }
+        
+        $sql = "DELETE FROM media
+                WHERE id_jeu = ?";
+        $req = $this->connection->prepare($sql);
+        $status = $req->execute(array($id));
+        return $status;
+    }
+    
+    public function addMedia($id, $lien){
+        $sql = "INSERT INTO media
+                (id_jeu, lien) VALUES (?, ?);";
+        $req = $this->connection->prepare($sql);
+        $status = $req->execute(array($id, $lien));
+        return $status;
+    }
+    
+    public function getMedias( $id )
+    {
+        $rows = $this->connection->query('SELECT lien
+                                          FROM media
+                                          WHERE id_media = '.$id)->fetchAll(\PDO::FETCH_OBJ);
+        $liens = [];
+        foreach ($rows as $row) {
+            $liens[] = $row->lien;
+        }
+        
+        return $liens;
+	}
+	
+	public function getMediasFromJeu( $id )
+	{
+		$res = $this->connection->query('SELECT * FROM media WHERE id_jeu = '.$id)->fetchAll(\PDO::FETCH_OBJ);
+		return $res;
+	}
 }
