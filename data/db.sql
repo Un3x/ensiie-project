17d1fb8b01c42648af91b527cfd5b400d984209d
Ouateu <40739330+Ouateu@users.noreply.github.com>
2019-05-14 14:42:02 +0200

    Push Groupe-T3

--- data/db.sql
+++ data/db.sql
@@ -1,19 +1,31 @@
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY ,
-    firstname VARCHAR NOT NULL ,
-    lastname VARCHAR NOT NULL ,
-    birthday date
+CREATE TABLE utilisateur(
+	id_pseudo VARCHAR PRIMARY KEY,
+	prenom VARCHAR NOT NULL,
+	nom VARCHAR NOT NULL,
+	mail VARCHAR NOT NULL,
+	pswd VARCHAR NOT NULL,
+	sexe VARCHAR NOT NULL,
+	birthdate DATE NOT NULL,
+	isAdmin BOOLEAN
 );
 
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('John', 'Doe', '1967-11-22');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Yvette', 'Angel', '1932-01-24');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Amelia', 'Waters', '1981-12-01');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Manuel', 'Holloway', '1979-07-25');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Alonzo', 'Erickson', '1947-11-13');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Otis', 'Roberson', '1995-01-09');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jaime', 'King', '1924-05-30');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Vicky', 'Pearson', '1982-12-12)');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Silvia', 'Mcguire', '1971-03-02');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Brendan', 'Pena', '1950-02-17');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jackie', 'Cohen', '1967-01-27');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Delores', 'Williamson', '1961-07-19');
+
+CREATE TABLE compte(
+	id_pseudo_compte VARCHAR PRIMARY KEY,
+	point_fort VARCHAR,
+	CONSTRAINT fk_id_pseudo FOREIGN KEY (id_pseudo_compte) REFERENCES utilisateur(id_pseudo)
+);
+
+
+
+CREATE TABLE aide(
+	id_aide SERIAL PRIMARY KEY,
+	pseudo_aide VARCHAR,
+	pseudo_aidant VARCHAR,
+	aide_matiere VARCHAR NOT NULL,
+	valide BOOLEAN,
+	jour DATE, 
+	heure VARCHAR,
+	numtel VARCHAR,
+	CONSTRAINT fk_aide FOREIGN KEY (pseudo_aide) REFERENCES compte(id_pseudo_compte)
+);
