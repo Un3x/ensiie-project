558a5281b4f0282aad40452297da3c3cc2dd3861
Nina HUANG <nina.huang@ensiie.fr>
2019-05-14 08:19:26 +0200

    discussions.php front OK

--- public/discussions.php
+++ public/discussions.php
@@ -15,37 +15,41 @@ $sujetsRepository = new SujetsRepository($connection);
     <body id="connected">
         <div class="container">
             <?php require('../src/components/navbar_connection.php');?>
-            <div id="sub-container">
-                <div id="sujet-list-container">
-                    <ul class="sujet-list">
-                        <?php
-                            $sujetArr = $sujetsRepository->getSujets();
-                            foreach($sujetArr as $s){
-                                $t = $s->getTitle();
-                                $a = $s->getAuthor();
-                                $c = $s->getContent();
-                                $rep = $s->getNbRep();
-                                echo "<li>";
-                                echo "<div>";
-                                echo "<h3 class='s-title'>$t</h3>";
-                                echo "<p class='s-content'>$c</p>";
-                                echo "<h5 class='s-author'>$a</h5>";
-                                echo "<p class='s-nbrep'>$rep</p>";
-                                echo "</div>";
-                                echo "<a class='vote-top' href='#'>&#9650;</a>";
-                                echo "<a class='vote-top' href='#'>&#9660;</a>";
-                                echo "</li>";
-                            }
-                        ?>
-                    </ul>
-                    <ul class="pagination"></ul>
-                </div>
-                <div id="sujet-create-container">
-                    <form method="post">
-                        <h3>Créer un nouveau sujet</h3>
-                        <input type="text" name="title" placeholder="Titre">
-                        <input type="text" name="content" placeholder="Description...">
-                    </form>
+            <div id="contain">
+                <h1>Questions</h1>
+                <div id="sub-container">
+                    <div id="sujet-list-container">
+                        <ul class="sujet-list">
+                            <?php
+                                $sujetArr = $sujetsRepository->getSujets();
+                                foreach($sujetArr as $s){
+                                    $t = $s->getTitle();
+                                    $a = $s->getAuthor();
+                                    $c = $s->getContent();
+                                    $rep = $s->getNbRep();
+                                    echo "<li>";
+                                    echo "<div>";
+                                    echo "<h3 class='s-title'>$t</h3>";
+                                    echo "<p class='s-content'>$c</p>";
+                                    echo "<h4 class='s-author'>$a</h4>";
+                                    echo "<p class='s-nbrep'>Réponses: $rep</p>";
+                                    echo "</div>";
+                                    echo "<a class='vote-top' href='#'>&#9650;</a>";
+                                    echo "<a class='vote-down' href='#'>&#9660;</a>";
+                                    echo "</li>";
+                                }
+                            ?>
+                        </ul>
+                        <ul class="pagination"></ul>
+                    </div>
+                    <div id="sujet-create-container">
+                        <form method="post">
+                            <h3>Créer un nouveau sujet</h3>
+                            <input type="text" name="title" placeholder="Titre">
+                            <input type="text" name="content" placeholder="Description...">
+                            <button type="submit">Création</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
