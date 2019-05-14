aa9fba3c8a7caa98db96affe142a6c8e25cc4261
Toro <corentinglh@gmail.com>
2019-05-14 12:11:46 +0200

    correction de failles XSS dans la recherche de courses

--- src/Controller/course/searchCourse.php
+++ src/Controller/course/searchCourse.php
@@ -1,7 +1,7 @@
 <?php
 
-$departure = isset($_GET['departure']) ? $_GET['departure'] : "";
-$arrival = isset($_GET['arrival']) ? $_GET['arrival'] : "";
+$departure = isset($_GET['departure']) ? htmlspecialchars($_GET['departure']) : "";
+$arrival = isset($_GET['arrival']) ? htmlspecialchars($_GET['arrival']) : "";
 $showResult = isset($_GET['departure'])&&isset($_GET['arrival']) ? "<script>searchCourse('departure', 'arrival', 'resDiv');</script>" : "";
 
 require('../src/View/course/searchCourseView.php');
